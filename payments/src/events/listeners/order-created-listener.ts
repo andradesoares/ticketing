139eeb6ae47848738e9170422bc70c714eaa17b2
Daniel Soares <danielandradesoares@hotmail.com>
2021-02-18 14:57:13 -0300

    Initial commit

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming'
+import { Subjects, Listener, OrderCreatedEvent } from '@dastickets/common'
+import { queueGroupName } from './queue-group-name'
+import { Order, OrderStatus } from '../../models/order'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated
+
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version
+    })
+    await order.save()
+    
+    msg.ack()
+  }
+}
---
+++ tickets/src/routes/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { requireAuth, validateRequest} from '@dastickets/common'
+import { Ticket } from '../models/ticket'
+
+const router = express.Router()
+
+router.get('/api/tickets', async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({
+    orderId: undefined
+  })
+  res.send(tickets)
+})
+
+export { router as indexTicketRouter }
